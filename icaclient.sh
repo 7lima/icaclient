3558503558f96722aa84df1b66dcd688b1b3f38a
Heiko Graeber <ip.5005@heiko-graeber.de>
2017-07-10 17:34:58 +0200

    Herausfinden der IP-Adresse mit besseren Mustern und Templates. Docker-Images von Docker-Compose-Images unterscheiden.

--- icaclient.sh
+++ icaclient.sh
@@ -18,12 +18,18 @@ if
 	sudo docker-compose --help >/dev/null	# ist es installiert?
 then
 	sudo docker-compose up -d		# dann per docker-compose
+	CONT=`sudo docker ps -q -f "name=${NAME}_${NAME}_*" | tail -n 1`
+						# ID des Containers
 else						# sonst per docker
 	sudo docker start $NAME 		|| \
 	sudo docker run --name $NAME 	   	   \
 		-e WEB_URL	  		   \
 		desktopcontainers/$NAME
+	CONT=`sudo docker ps -q -f name"=$NAME" | tail -n 1`
+						# ID des Containers
 fi || exit
 
-ADRESSE=`sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $NAME`
-ssh -fX app@$ADRESSE true
+ADRE=`sudo docker inspect \
+	-f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONT`
+						# IP-Adresse des Containers
+ssh -fX app@$ADRE true				# SSH startet immer Firefox
