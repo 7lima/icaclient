51ff8a619e829fb58e0aae78e7a34af9c84155be
Heiko Graeber <ip.5005@heiko-graeber.de>
2017-07-18 10:19:45 +0200

    Nicht mehr sudo verwenden.

--- icaclient.sh
+++ icaclient.sh
@@ -19,21 +19,21 @@ NAME=icaclient
 cd `dirname "$0"` || exit			# hier liegt docker-compose.yml
 
 if
-	sudo docker-compose --help >/dev/null	# ist es installiert?
+	docker-compose --help >/dev/null	# ist es installiert?
 then
-	sudo docker-compose up -d		# dann per docker-compose
-	CONT=`sudo docker ps -q -f "name=${NAME}_${NAME}_*" | tail -n 1`
+	docker-compose up -d			# dann per docker-compose
+	CONT=`docker ps -q -f "name=${NAME}_${NAME}_*" | tail -n 1`
 						# ID des Containers
 else						# sonst per docker
-	sudo docker start $NAME 			|| \
-	sudo docker run --name $NAME 	   		   \
+	docker start $NAME 				|| \
+	docker run --name $NAME 	   		   \
 		-e WEB_URL -P	  			   \
 		7lima/$NAME
-	CONT=`sudo docker ps -q -f name"=$NAME" | tail -n 1`
+	CONT=`docker ps -q -f name"=$NAME" | tail -n 1`
 						# ID des Containers
 fi || exit
 
-ADRE=`sudo docker inspect \
+ADRE=`docker inspect \
 	-f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONT`
 						# IP-Adresse des Containers
 ssh -fX							\
